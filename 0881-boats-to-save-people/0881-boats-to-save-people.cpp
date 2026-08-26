99b2b1fd5d3145997a4fa7c085699202b64553c2
Mohammad-Fahim05 <fahimfiroz2005@gmail.com>
2026-08-26 12:28:23 +0530

    Time: 17 ms (59.1%), Space: 45.9 MB (38.49%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -2,18 +2,18 @@ class Solution {
 public:
     int numRescueBoats(vector<int>& people, int limit) {
         sort(people.begin() , people.end());
-        int count = 0;
         int left = 0;
         int right = people.size() -1 ;
-
+        int count = 0;
         while(left <= right){
             if(people[left] + people[right] <= limit){
+                count++;
                 left ++;
                 right--;
-                count++;
-            }else 
-            {right--;
-            count++;}
+            }
+            else 
+            {count++;
+            right--;}
         }
         return count;
     }
