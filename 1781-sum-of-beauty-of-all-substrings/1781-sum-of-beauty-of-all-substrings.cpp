9537e6f47afb57a44d5cb8790681d0aec9a74478
Mohammad-Fahim05 <fahimfiroz2005@gmail.com>
2026-08-28 12:01:19 +0530

    Time: 111 ms (61.55%), Space: 11 MB (73.98%) - LeetHub

--- 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
+++ 1781-sum-of-beauty-of-all-substrings/1781-sum-of-beauty-of-all-substrings.cpp
@@ -3,19 +3,21 @@ public:
     int beautySum(string s) {
         int n = s.size();
         int ans = 0;
-        for(int i = 0;i < n;i++){
-            unordered_map<char, int > mp;
-            for(int j =  i; j< n; j++){
-                mp[s[j]]++;
-    
-            int maxi = INT_MIN;
-            int mini = INT_MAX;
-            for(auto ch : mp){
-                 maxi = max(maxi, ch.second);
-                 mini = min(mini , ch.second);
+        for (int i = 0; i < n; i++) {
+            vector<int> freq(26,0);
+            for (int j = i; j < n; j++) {
+                freq[s[j] - 'a']++;
+
+                int maxi = INT_MIN;
+                int mini = INT_MAX;
+                for (int k = 0; k < 26; k++) {
+                    if (freq[k] > 0) {
+                        maxi = max(maxi, freq[k]);
+                        mini = min(mini, freq[k]);
+                    }
+                }
+                ans += maxi - mini;
             }
-            ans += maxi - mini ;   
-            } 
         }
         return ans;
     }
