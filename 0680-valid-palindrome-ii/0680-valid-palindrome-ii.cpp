5ccb5d78f1508757dfa589349541ea56c5fcd425
Mohammad-Fahim05 <fahimfiroz2005@gmail.com>
2026-09-11 17:49:04 +0530

    Time: 4 ms (56.44%), Space: 26.7 MB (24.88%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+
+      bool solve(int i , int j , string s){
+            while(i < j){
+                if(s[i] == s[j]){
+                    i++;
+                    j--;
+                }else return false;
+            } 
+            return true;
+        }
+    bool validPalindrome(string s) {
+        int i =0;
+        int j = s.size() -1;
+        int count = 0;
+        while(i <= j){
+            if(s[i] == s[j]){
+                i++;
+                j--;
+            }
+            else{
+                return solve(i+1, j , s) ||  solve(i, j-1, s);
+            }
+        }
+        return true;
+    }
+};
